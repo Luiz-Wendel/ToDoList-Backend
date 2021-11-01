c05dedd0d45d214ed840399346f827a9a2886e89
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-01 17:29:50 -0300

    test: add taskController.getAll tests when it has no tasks

---
+++ src/__tests__/mock/data.js
@@ -0,0 +1,19 @@
+module.exports = {
+  tasks: [
+    {
+      description: 'Meet parents',
+      createdAt: '1635797574893',
+      status: 'Pending',
+    },
+    {
+      description: 'Buy food',
+      createdAt: '1635797574894',
+      status: 'In progress',
+    },
+    {
+      description: 'Leave kids at school',
+      createdAt: '1635797574895',
+      status: 'Done',
+    },
+  ],
+};
--- src/__tests__/unit/models.js
+++ src/__tests__/unit/models.js
@@ -1,6 +1,57 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-describe('todoController', () => {
-  describe('getAll');
+const mongoConnection = require('../../models/connection');
+const TaskModel = require('../../models/TaskModel');
+
+describe('taskController', () => {
+  describe('getAll', () => {
+    const DBServer = new MongoMemoryServer();
+    let connectionMock;
+
+    before(async () => {
+      const URLMock = await DBServer.getUri();
+
+      connectionMock = await MongoClient
+        .connect(URLMock, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
+        .then((conn) => conn.db('tests'));
+
+      sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+    });
+
+    after(() => {
+      mongoConnection.getConnection.restore();
+    });
+
+    describe('when it has no tasks', () => {
+      it('should return an object', async () => {
+        const response = await TaskModel.getAll();
+
+        expect(response).to.be.an('object');
+      });
+
+      it('should have the "tasks" property', async () => {
+        const response = await TaskModel.getAll();
+
+        expect(response).to.have.a.property('tasks');
+      });
+
+      it('should have the "taks" property as an array', async () => {
+        const response = await TaskModel.getAll();
+
+        expect(response.tasks).to.be.an('array');
+      });
+
+      it('should have the "tasks" property with an empty array', async () => {
+        const response = await TaskModel.getAll();
+
+        expect(response.tasks).to.have.lengthOf(0);
+      });
+    });
+  });
 });
