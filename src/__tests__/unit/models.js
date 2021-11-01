558b14d4d3e87968aa3643d802420d33e56d92da
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-01 17:30:30 -0300

    test: add taskController.getAll tests when it has tasks

--- src/__tests__/unit/models.js
+++ src/__tests__/unit/models.js
@@ -3,6 +3,8 @@ const { expect } = require('chai');
 const { MongoClient } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
+const mockData = require('../mock/data');
+
 const mongoConnection = require('../../models/connection');
 const TaskModel = require('../../models/TaskModel');
 
@@ -51,6 +53,55 @@ describe('taskController', () => {
         const response = await TaskModel.getAll();
 
         expect(response.tasks).to.have.lengthOf(0);
+      });
+    });
+
+    describe('when it has tasks', () => {
+      const tasks = [...mockData.tasks];
+
+      before(async () => {
+        const asyncTaskInsertions = [];
+
+        tasks.forEach((task) => {
+          asyncTaskInsertions.push(connectionMock.collection('tasks').insertOne(task));
+        });
+
+        const taskIds = await Promise.all(asyncTaskInsertions);
+
+        taskIds.forEach((id, index) => {
+          tasks[index] = { ...tasks[index], _id: id };
+        });
+      });
+
+      after(async () => {
+        await connectionMock.collection('tasks').deleteMany({});
+      });
+
+      it('should return an object', async () => {
+        const response = await TaskModel.getAll();
+
+        expect(response).to.be.an('object');
+      });
+
+      it('should have the "tasks" property', async () => {
+        const response = await TaskModel.getAll();
+
+        expect(response).to.have.a.property('tasks');
+      });
+
+      it('should have the tasks property as an array', async () => {
+        const response = await TaskModel.getAll();
+
+        expect(response.tasks).to.be.an('array');
+      });
+
+      it('should have the tasks property with the array of tasks', async () => {
+        const response = await TaskModel.getAll();
+
+        expect(response.tasks).to.have.lengthOf(tasks.length);
+        response.tasks.forEach((task, index) => {
+          expect(task).to.deep.equal(tasks[index]);
+        });
       });
     });
   });
