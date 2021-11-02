8c0e0f892e82e6c5584c37e2eaa3cd0c3cf143bf
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-02 11:59:33 -0300

    feat: add _id property to mocked tasks

--- src/__tests__/mock/data.js
+++ src/__tests__/mock/data.js
@@ -1,16 +1,19 @@
 module.exports = {
   tasks: [
     {
+      _id: '507f1f77bcf86cd799439011',
       description: 'Meet parents',
       createdAt: '1635797574893',
       status: 'Pending',
     },
     {
+      _id: '507f1f77bcf86cd799439012',
       description: 'Buy food',
       createdAt: '1635797574894',
       status: 'In progress',
     },
     {
+      _id: '507f1f77bcf86cd799439013',
       description: 'Leave kids at school',
       createdAt: '1635797574895',
       status: 'Done',
