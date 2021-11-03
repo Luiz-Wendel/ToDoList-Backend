c2ee9fef91a9414c205086662f646c385f0f1e52
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-02 22:35:47 -0300

    feat: add task creation route

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const taskController = require('../controllers/taskController');
 
+const taskValidator = require('../middlewares/validators/taskValidator');
+
 const taskRouter = express.Router();
 
 taskRouter.get('/', taskController.getAll);
+taskRouter.post('/', taskValidator, taskController.create);
 
 module.exports = taskRouter;
