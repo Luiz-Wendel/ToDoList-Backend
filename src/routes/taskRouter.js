b5503474eefe48546e77591d339b89858fdb76c1
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-03 00:21:54 -0300

    feat: add task deletion route

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -3,10 +3,12 @@ const express = require('express');
 const taskController = require('../controllers/taskController');
 
 const taskValidator = require('../middlewares/validators/taskValidator');
+const mongoIdValidator = require('../middlewares/validators/mongoIdValidator');
 
 const taskRouter = express.Router();
 
 taskRouter.get('/', taskController.getAll);
 taskRouter.post('/', taskValidator, taskController.create);
+taskRouter.delete('/:id', mongoIdValidator, taskController.remove);
 
 module.exports = taskRouter;
