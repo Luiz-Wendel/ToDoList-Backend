b3ad9087c74152c5f387e3ad91bf8d0df5a81f62
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-03 19:08:39 -0300

    feat: add jwt validator middleware to tasks routes

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const taskController = require('../controllers/taskController');
 
+const jwtValidator = require('../middlewares/auth/jwtValidator');
 const newTaskValidator = require('../middlewares/validators/newTaskValidator');
 const taskValidator = require('../middlewares/validators/taskValidator');
 const statusValidator = require('../middlewares/validators/statusValidator');
 const mongoIdValidator = require('../middlewares/validators/mongoIdValidator');
 
 const taskRouter = express.Router();
+
+taskRouter.use(jwtValidator);
 
 taskRouter.get('/', taskController.getAll);
 taskRouter.post('/', newTaskValidator, taskController.create);
