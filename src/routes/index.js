6f5fc0bdc59354bc5a0359e748dd62b5cd525090
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-03 17:17:12 -0300

    feat: link user router to root router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const taskRouter = require('./taskRouter');
+const userRouter = require('./userRouter');
 
 const rootRouter = express.Router();
 
 rootRouter.use('/tasks', taskRouter);
+rootRouter.use('/users', userRouter);
 
 module.exports = rootRouter;
