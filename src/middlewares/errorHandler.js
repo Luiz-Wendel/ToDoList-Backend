989262847fdf7f6ae487a8ad7adf9ceeab86bdc8
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-02 22:35:38 -0300

    feat: add Joi error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,16 @@
 const statusCodes = require('../schemas/statusCodesSchema');
 
-module.exports = (err, _req, res, _next) => res
-  .status(statusCodes.internalServerError)
-  .json(err);
+module.exports = (err, _req, res, _next) => {
+  let error = {};
+
+  if (err.isJoi) {
+    error = {
+      code: 'precondition_failed',
+      message: err.details[0].message,
+    };
+
+    return res.status(statusCodes.preconditionFailed).json(error);
+  }
+
+  return res.status(statusCodes.internalServerError).json(err);
+};
