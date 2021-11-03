25e99b020a55a89f0c4b5a02134102f6b824ce0e
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-03 11:13:09 -0300

    feat: add task not updated error

--- src/schemas/errorsSchema.js
+++ src/schemas/errorsSchema.js
@@ -7,5 +7,10 @@ module.exports = {
       code: 'not_found',
       message: 'Task not found',
     },
+    notUpdated: {
+      statusCode: statusCodes.preconditionFailed,
+      code: 'not_updated',
+      message: 'Task not found or has nothing to update',
+    },
   },
 };
